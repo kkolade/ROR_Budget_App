038e19400298bee48cf00b9e7f34ee8f50e963bc
Kola Kolade <kolakolade@gmail.com>
2023-08-14 19:02:13 +0100

    Generate resources

---
+++ app/views/bills/_bill.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! bill, :id, :name, :icon, :created_at, :updated_at
+json.url bill_url(bill, format: :json)
---
+++ db/migrate/20230814180152_create_bills.rb
@@ -0,0 +1,10 @@
+class CreateBills < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bills do |t|
+      t.string :name
+      t.string :icon
+
+      t.timestamps
+    end
+  end
+end
