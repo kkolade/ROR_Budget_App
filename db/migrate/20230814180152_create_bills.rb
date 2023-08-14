68aafb16f5dee90f9e750c2b79ef5ac17cea3f7c
Kola Kolade <kolakolade@gmail.com>
2023-08-14 19:08:32 +0100

    Edit migration files to include association

--- db/migrate/20230814180152_create_bills.rb
+++ db/migrate/20230814180152_create_bills.rb
@@ -3,6 +3,7 @@ class CreateBills < ActiveRecord::Migration[7.0]
     create_table :bills do |t|
       t.string :name
       t.string :icon
+      t.references :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
--- db/migrate/20230814180175_create_items.rb
+++ db/migrate/20230814180175_create_items.rb
@@ -3,6 +3,8 @@ class CreateItems < ActiveRecord::Migration[7.0]
     create_table :items do |t|
       t.string :name
       t.decimal :amount
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.references :bill, null: false, foreign_key: true
 
       t.timestamps
     end
